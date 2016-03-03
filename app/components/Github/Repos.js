126ba738bde3d70060b44dd4908da244388eff79
bhuber2010 <bhuber@alumni.colostate.edu>
2016-03-02 23:27:38 -0700

    first react app built and working

---
+++ app/components/Github/Repos.js
@@ -0,0 +1,29 @@
+var React = require('react');
+
+var Repos = React.createClass({
+  propTypes: {
+    username: React.PropTypes.string.isRequired,
+    repos: React.PropTypes.array.isRequired
+  },
+  render: function(){
+    console.log('Repos: ', this.props.repos);
+    var repos =this.props.repos.map(function(repo, i){
+      return (
+        <li className="list-group-item" key={i}>
+          {repo.html_url && <h4><a href={repo.html_url}>{repo.name}</a></h4>}
+          {repo.description && <p>{repo.description}</p>}
+        </li>
+      )
+    });
+    return (
+      <div>
+        <h3>User Repos</h3>
+        <ul className="list-group">
+          {repos}
+        </ul>
+      </div>
+    )
+  }
+});
+
+module.exports = Repos;
